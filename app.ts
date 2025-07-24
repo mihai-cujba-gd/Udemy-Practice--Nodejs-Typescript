6497ad0376f76c2d30b0701aa35ea9e099c6df27
Mihai Cujba <mcujba@griddynamics.com>
2025-07-24 13:04:01 +0300

    feat: created nodejs server

--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("Hello world!")
+import { createServer } from "node:http";
+
+const server = createServer((req, res) => {
+    console.log(req.method);
+
+    res.end("Hello world!")
+});
+
+server.listen(3000);
